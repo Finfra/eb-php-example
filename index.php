85c54d3339a00cae748908509042557ca7fbd816
vagrant <vagrant@terraformBox>
2019-05-31 22:23:22 +0000

    hi

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php 
 echo '<p>Hello world!</p>';
-echo '<h1>hi</h1>';
+echo '<h1>hi</h1>
 $mysqli = new mysqli(getenv('RDS_HOSTNAME'), getenv('RDS_USERNAME'), getenv('RDS_PASSWORD'), getenv('RDS_DATABASE'));
 if ($mysqli->connect_errno) {
       echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
